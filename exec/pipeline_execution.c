ec93adce9e5dd33ed55ad74f2b90a908a03212d7
Chaimaa5 <chaimaa.elmhandez@gmail.com>
2022-08-22 06:05:54 +0100

    BUILTINS

--- exec/pipeline_execution.c
+++ exec/pipeline_execution.c
@@ -15,13 +15,13 @@ void 	exec_builtins(t_parser *parser, t_env_list *env)
 {
 	if (!ft_strncmp(parser->cmd, "cd", 3))
 		exec_cd(parser->args[1], env);
-    else if (!ft_strncmp(parser->cmd, "pwd", 4))
+    else if (!ft_strncmp(parser->cmd, "pwd", 4) || !ft_strncmp(parser->cmd, "PWD", 4))
         exec_pwd();
-	else if (!ft_strncmp(parser->cmd, "echo", 6))
+	else if (!ft_strncmp(parser->cmd, "echo", 6) || !ft_strncmp(parser->cmd, "ECHO", 6))
         exec_echo(parser);
     else if (!ft_strncmp(parser->cmd, "exit", 6))
         exec_exit(parser);
-    else if (!ft_strncmp(parser->cmd, "env", 4))
+    else if (!ft_strncmp(parser->cmd, "env", 4) || !ft_strncmp(parser->cmd, "ENV", 4))
         exec_env(parser, &env);
 	else if (!ft_strncmp(parser->cmd, "export", 8))
         exec_export(parser, &env);
@@ -29,61 +29,62 @@ void 	exec_builtins(t_parser *parser, t_env_list *env)
         exec_unset(&env, parser->args[1]);
 }
 
-void 	execute(t_parser *parser, char *path, char **envp, t_env_list *env)
-{
-	if (check_builtin(parser))
-        exec_builtins(parser, env);
-	else if (execve(path, parser->args, envp) == -1)
-	{
-		ft_putstr_fd("command not found: ", 2);
-		ft_putendl_fd(parser->cmd, 2);
-		exit(127);
-	}
-	exit(0);
-}
 
-void execute_last_cmd(t_parser *parser, t_env_list *env, int write_in, int file)
+void execute_last_cmd(t_parser *parser, t_env_list *env, int fd_in, int file)
 {
 	char	*path;
 	char	**envp;
 	int pid;
-	int status;
 
 	envp = t_env_list_to_char(&env);
 	path = search(envp, parser->cmd);
 	pid = fork();
-	waitpid(pid, &status, 0);
 	if (pid == 0)
 	{
-		if (write_in != 0)
+		if (fd_in != 0)
 		{
-			dup2(write_in, STDIN_FILENO);
-			close(write_in);
+			dup2(fd_in, STDIN_FILENO);
+			close(fd_in);
 		}
 		redirections(parser->red, parser->cmd, file);
-		if (parser->cmd && !(check_builtin(parser)))
-			execute(parser, path, envp, env);
+		if (parser->cmd)
+		{
+			if (check_builtin(parser) && parser->cmd)
+			{
+        		exec_builtins(parser, env);
+				return ;
+			}
+			else if (execve(path, parser->args, envp) != -1)
+			{
+				ft_putstr_fd("command not found: ", 2);
+				ft_putendl_fd(parser->cmd, 2);
+				exit(127);
+			}
+		}
 		exit(0);
 	}
-	if (check_builtin(parser))
-		exec_builtins(parser, env);
 }
-void	launch_child(t_parser *parser, t_env_list *env, int write_in, int *end, int file)
+void	launch_child(t_parser *parser, t_env_list *env, int fd_in, int *end, int file)
 {
 	char	*path;
 	char	**envp;
 	envp = t_env_list_to_char(&env);
 	path = search(envp, parser->cmd);
-	if (write_in != 0)
+	if (fd_in != 0)
 	{
-		dup2(write_in, STDIN_FILENO);
-		close(write_in);
+		dup2(fd_in, STDIN_FILENO);
+		close(fd_in);
 	}
 	dup2(end[WRITE], STDOUT_FILENO);
 	close(end[WRITE]);
 	redirections(parser->red, parser->cmd, file);
-	if (parser->cmd)
-		execute(parser, path, envp, env);
+	if (check_builtin(parser) && parser->cmd)
+        exec_builtins(parser, env);	
+	else if (parser->cmd && (execve(path, parser->args, envp) != -1))
+	{
+		ft_putstr_fd("command not found: ", 2);
+		ft_putendl_fd(parser->cmd, 2);
+	}
 	exit(0);
 }
 
@@ -92,22 +93,22 @@ void    pipeline_execution(t_parser *parser, t_env_list **envp, int file)
 	pid_t pid;
 	t_env_list *env;
 	int end[2];
-	int	write_in;
+	int	fd_in;
 	int status;
 	
-	write_in = 0;
+	fd_in = 0;
 	env = (*envp);
 	while (parser->next)
 	{
 		pipe(end);
 		pid = fork();
 		if (pid == 0)
-			launch_child(parser, env, write_in, end, file);
-		write_in = end[READ];
+			launch_child(parser, env, fd_in, end, file);
+		fd_in = end[READ];
 		close(end[WRITE]);
 		parser = parser->next;
 	}
-	execute_last_cmd(parser, env, write_in, file);
+	execute_last_cmd(parser, env, fd_in, file);
 	while(waitpid(-1, &status, 0) > 0);
 }
 
--- lexer/collect_lexer.c
+++ lexer/collect_lexer.c
@@ -7,15 +7,12 @@ char *add_dolar(t_lexer *lexer)
 
 	value = ft_strdup("");
 	lexer_advance(lexer);
-	if(ft_isalpha(lexer->c) || lexer->c == '_')
-	{
-		while (lexer->c != ' ' && lexer->c != '\0' && lexer->c != '|' && lexer->c != '>' && lexer->c != '<' && lexer->c != '$' && lexer->c != '\'' && lexer->c != '"' )
+		while ( (lexer->c != '\0') && (ft_isalpha(lexer->c) || lexer->c == '_' || ft_isalnum(lexer->c)))
 		{
 			s = get_char_as_string(lexer);
 			value = ft_strjoin(value, s);
 			lexer_advance(lexer);
 		}
-	}
 	return(value);
 }
 
@@ -41,7 +38,7 @@ char *join_to_join(t_lexer *lexer, char c, t_env_list *env)
 
 	value = ft_strdup("");
 	join = ft_strdup("");
-	if(lexer->c == '"')
+	if(c == '"')
 		q = 1;
 	lexer_advance(lexer);
 	while(lexer->c != c && lexer->c != '\0')
@@ -52,11 +49,13 @@ char *join_to_join(t_lexer *lexer, char c, t_env_list *env)
 			if(add_dolar_token(join, env))
 				value = ft_strjoin(value, add_dolar_token(join, env));
 			free(join);
-			return (value);
 		}
-		s = get_char_as_string(lexer);
-		value = ft_strjoin(value, s);
-		lexer_advance(lexer);
+		if(lexer->c != c)
+		{
+			s = get_char_as_string(lexer);
+			value = ft_strjoin(value, s);
+			lexer_advance(lexer);
+		}
 	}
 	return(value);
 }
@@ -95,16 +94,11 @@ t_token *collect_cmd(t_lexer *lexer, t_env_list *env)
 	char *value;
 	char *s;
 	char *join;
+	int i;
+	i = 0;
 
 	join = ft_strdup("");
 	value = ft_strdup("");
-	if(lexer->c == '$')
-	{
-		join = add_dolar(lexer);
-		if(add_dolar_token(join, env))
-			value = ft_strjoin(value, add_dolar_token(join, env));
-		free(join);
-	}
 	while (lexer->c != ' ' && lexer->c != '\0' && lexer->c != '|' && lexer->c != '>' && lexer->c != '<')
 	{
 		if(lexer->c != '"' && lexer->c != '\'')
@@ -114,12 +108,17 @@ t_token *collect_cmd(t_lexer *lexer, t_env_list *env)
 				join = add_dolar(lexer);
 				if(add_dolar_token(join, env))
 					value = ft_strjoin(value, add_dolar_token(join, env));
+				if(lexer->c == '"' || lexer->c == '\'' || lexer->c == '$')
+					i = 1;
 				free(join);
 			}
+			if(!i){
 			s = get_char_as_string(lexer);
-			value = ft_strjoin(value, s);
+			value = ft_strjoin(value, s);}
 		}
-		lexer_advance(lexer);
+		if(!i)
+			lexer_advance(lexer);
+		i = 0;
 		if(lexer->c == '"' || lexer->c == '\'')
 		{
 			join = ft_strjoin(join, join_to_join(lexer, lexer->c, env));
@@ -192,7 +191,7 @@ t_token *collect_apn_hrd(t_lexer *lexer, int i)
 	return(init_token(i, value));
 }
 
-t_token *collect_string(t_lexer *lexer, t_env_list *env)
+char *add_dolar_and_after_q(t_lexer *lexer, t_env_list *env)
 {
 	char *value;
 	char *join;
@@ -200,7 +199,8 @@ t_token *collect_string(t_lexer *lexer, t_env_list *env)
 
 	join = ft_strdup("");
 	value = ft_strdup("");
-	while (lexer->c != '|' && lexer->c != '>' && lexer->c != '<' && lexer->c != '\0' && lexer->c != ' ')
+
+	while(lexer->c != '|' && lexer->c != '>' && lexer->c != '<' && lexer->c != '\0' && lexer->c != ' ')
 	{
 		if(lexer->c == '$')
 		{
@@ -214,6 +214,30 @@ t_token *collect_string(t_lexer *lexer, t_env_list *env)
 			s = get_char_as_string(lexer);
 			value = ft_strjoin(value, s);
 		}
+		lexer_advance(lexer);
+	}
+	return(value);
+}
+
+t_token *collect_string(t_lexer *lexer, t_env_list *env)
+{
+	char *value;
+	char *demo;
+	char *join;
+
+	join = ft_strdup("");
+	value = ft_strdup("");
+	demo = NULL;
+	while (lexer->c != '|' && lexer->c != '>' && lexer->c != '<' && lexer->c != '\0' && lexer->c != ' ')
+	{
+		
+		if((lexer->c == '$') || (lexer->c != '"' && lexer->c != '\''))
+			demo = add_dolar_and_after_q(lexer, env);
+		if(demo)
+		{
+			value = ft_strjoin(value, demo);
+			free(demo);
+		}
 		if(lexer->c == '"' || lexer->c == '\'')
 		{
 			join = ft_strjoin(join, join_to_join(lexer, lexer->c, env));
@@ -222,6 +246,7 @@ t_token *collect_string(t_lexer *lexer, t_env_list *env)
 		}
 		lexer_advance(lexer);
 	}
-	value = add_value(value);
+	if(value)
+		value = add_value(value);
 	return(init_token(TOKEN_STR, value));
 }
